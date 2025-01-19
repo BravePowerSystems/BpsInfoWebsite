ca3189b6cc5673fff530c78e505a7b8d01a33f08
M SAI NITHIN <nithin6524@gmail.com>
2025-01-19 19:35:14 +0530

    frontend setup done

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import "./scss/main.scss";
+function App() {
+  return (
+    <div className="App">
+      
+          Learn React
+    </div>
+  );
+}
+
+export default App;
