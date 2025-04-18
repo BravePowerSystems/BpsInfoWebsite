4b20acff98d466ecf79d7f858301ff09e168af02
M SAI NITHIN <nithin6524@gmail.com>
2025-04-19 01:10:26 +0530

    added colorcursor, herosection

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,15 +1,23 @@
 import React from "react";
 import "../scss/components/Breadcrumbs.scss";
 import "../scss/pages/Home.scss";
-import PipelineBackground from "../components/PipelineBackground";
 import { motion } from "framer-motion";
+import HeroSection from "../components/HeroSection";
+
 function Home() {
     return (
-        <motion.div className="home"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1, transition: { duration: 2, delay: 0.5 } }}>
-            <PipelineBackground />
-        </motion.div>
+        <>
+            <motion.div
+                className="home"
+                initial={{ opacity: 0 }}
+                animate={{
+                    opacity: 1,
+                    transition: { duration: 2, delay: 0.5 },
+                }}
+            >
+                <HeroSection />
+            </motion.div>
+        </>
     );
 }
 export default Home;
