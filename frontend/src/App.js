c54a1c5adfff135ce4bc87725219b42c25f47b5d
M SAI NITHIN <nithin6524@gmail.com>
2025-04-19 01:13:27 +0530

    small changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,11 @@ import Layout from "./Layout/Layout";
 import Products from "./Pages/Products";
 import Home from "./Pages/Home";
 import Blog from "./Pages/Blog";
-import CaseStudyDetails from "./Pages/CaseStudy";
+import CaseStudy from "./Pages/CaseStudy";
 import Product from "./Pages/Product";
 import Wishlist from "./Pages/Wishlist";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./scss/main.scss";  
+import "./scss/main.scss"; //importing the main.scss file to apply the styles to the app.js file.
 import CategoryPage from "./Pages/CategoryPage";
 
 function App() {
@@ -15,15 +15,24 @@ function App() {
         <Router>
             <Layout>
                 <div className="App">
-                        <Routes>
-                            <Route path="/" element={<Home />} />
-                            <Route path="/Products" element={<Products />} />
-                            <Route path="/Products/:categoryName"element={<CategoryPage />} />
-                            <Route path="/Products/:categoryName/:productName"element={<Product />}/>
-                            <Route path="/Blog" element={<Blog />} />
-                            <Route path="/CaseStudyDetails"element={<CaseStudyDetails />} />
-                        <Route path="/Wishlist" element={<Wishlist/>} />
-                        </Routes>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/Products" element={<Products />} />
+                        <Route
+                            path="/Products/:categoryName"
+                            element={<CategoryPage />}
+                        />
+                        <Route
+                            path="/Products/:categoryName/:productName"
+                            element={<Product />}
+                        />
+                        <Route path="/Blog" element={<Blog />} />
+                        <Route
+                            path="/case-studies"
+                            element={<CaseStudy />}
+                        />
+                        <Route path="/Wishlist" element={<Wishlist />} />
+                    </Routes>
                 </div>
             </Layout>
         </Router>
