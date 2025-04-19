d2476bf35a6cdbb3a707ac3cfef8a6e8b9f7d6d0
M SAI NITHIN <nithin6524@gmail.com>
2025-04-19 20:44:14 +0530

    small changes in the styles

--- frontend/src/Layout/Layout.js
+++ frontend/src/Layout/Layout.js
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import TopNav from "../components/TopNav";
 import BottomNav from "../components/BottomNav";
 import ColorCursor from "../components/ColorCursor";
+import ProductModal from "../components/ProductModal";
+import { useLocation } from "react-router-dom";
+import { ModalProvider, useModal } from "../context/ModalContext";
 import "../scss/layouts/layout.scss";
- function Layout({ children }) {
+
+const LayoutContent = ({ children }) => {
+    const layoutRef = useRef(null);
+    const location = useLocation();
+    const { productModal, closeProductModal } = useModal();
+
+    useEffect(() => {
+        if (layoutRef.current) {
+            layoutRef.current.scrollTo({
+                top: 0,
+                left: 0,
+            });
+        }
+    }, [location.pathname]);
+
     return (
-        <div className="layout">
+        <div className="layout" ref={layoutRef}>
             <ColorCursor />
-                <TopNav />
+            <TopNav />
             {children}
-                <BottomNav />
+            <BottomNav />
+            {productModal.isOpen && (
+                <ProductModal 
+                    productName={productModal.productName}
+                    onClose={closeProductModal}
+                />
+            )}
         </div>
+    );
+};
+
+function Layout({ children }) {
+    return (
+        <ModalProvider>
+            <LayoutContent>{children}</LayoutContent>
+        </ModalProvider>
     );
 }
 
